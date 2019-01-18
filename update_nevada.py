545793f5ae109d9019dc08a815d57e7759dce81c
Robb Shecter <robb@public.law>
2019-01-18 15:56:36 -0800

    docstrings

--- update_nevada.py
+++ update_nevada.py
@@ -1,26 +1,30 @@
 #!/bin/env python3.7
 
-# A script, meant to be run from cron, which parses
-# the current Nevada laws and commits the result to
-# the data repository.
+"""
+A script, meant to be run from cron, which parses
+the current Nevada laws and commits the result to
+the data repository.
+"""
 
 import os
 from typing import List
 
 
 def run_all(commands: List[str]):
+    """Execute a list of commands, breaking on error"""
     for cmd in commands:
         run(cmd)
 
 
 def run(command: str):
+    """Execute a command, raising Exception if it has an error"""
     if os.system(command) != 0:
         raise Exception("Error running " + command)
 
 
 run_all([
-        "docker run publiclaw/nevada-nrs > usa/nevada/nrs.json",
-        "git add usa/nevada/nrs.json",
-        "git commit -m 'Daily Nevada update'",
-        "git push origin master"
-        ])
+    "docker run publiclaw/nevada-nrs > usa/nevada/nrs.json",
+    "git add usa/nevada/nrs.json",
+    "git commit -m 'Daily Nevada update'",
+    "git push origin master"
+])
